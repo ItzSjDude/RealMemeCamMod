af95233f3ea329402642171c86c397581e5d21d8
itzsjdude <itzsjdude@users.noreply.github.com>
2026-03-23 00:37:24 +0530

    Initial commit: Realmeme Camera Mod for AOSP

---
+++ realmeme.mk
@@ -0,0 +1,66 @@
+# Realmeme Camera Mod - Product Config inclusion setup
+
+# Add Framework extension archives to Boot Jars to load on startup
+PRODUCT_BOOT_JARS += \
+    oplus-framework \
+    oplus-support-wrapper
+
+
+# Soong Namespaces for this module
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/realmeme_cam_mod
+
+
+# --- Auto-Generated PRODUCT_PACKAGES list ---
+PRODUCT_PACKAGES += \
+    libarcsoft_hdr_denoise_api \
+    libarcsoft_dualcam_refocus_uw \
+    libstfaceunlockocl \
+    libAlgoProcess \
+    libarcsoft_hdr_couple_api \
+    libarcsoft_dualcam_bokeh_frt_api \
+    libarcsoft_low_light_hdr \
+    libarcsoft_frc \
+    libarcsoft_dualcam_refocus_frt_image \
+    libarcsoft_super_night_raw \
+    libAlgoInterface \
+    libarcsoft_high_dynamic_range \
+    libarcsoft_dualcam_refocus_right \
+    libarcsoft_hdrplus_hvx_stub \
+    libarcsoft_scbokeh_video \
+    libarcsoft_smart_denoise \
+    libarcsoft_portrait_super_night_raw \
+    libstface_fd_api \
+    liboplus_service \
+    libarcsoft_dualcam_bokeh_api \
+    libarcsoft_portraitSN_hvx_stub \
+    libmegface \
+    libarcsoft_dualcam_refocus_frt_preview \
+    libarcsoft_dualcam_refocus_frt_uw \
+    libarcsoft_high_dynamic_range_couple \
+    libarcsoft_dualcam_refocus_left \
+    libarcsoft_distortion_correction \
+    libarcsoft_lensstaindetection \
+    libarcsoft_dualcam_refocus_preview \
+    libarcsoft_scbokeh_image \
+    libarcsoft_portraitSN_hvx_skel \
+    libarcsoft_dualcam_refocus_skel \
+    libarcsoft_portraitsn_hvx_5221_skel \
+    libarcsoft_hdrplus_hvx_5221_skel \
+    libarcsoft_dualcam_refocus_5221_skel \
+    libarcsoft_hdrplus_hvx_skel \
+    libcamera_nn_stub \
+    libcamera_nn_skel \
+    projectname \
+    video_beauty_default_config \
+    imx682 \
+    gc02k0 \
+    oplus_camera_algo_switch_config \
+    ov02b \
+    ov32a1q \
+    hi846 \
+    oplus_camera_config \
+    camera_unit_config \
+    oplus_camera_aps_config \
+    vendor.oplus.hardware.orms.ormsHalService@1.0-service \
+    
