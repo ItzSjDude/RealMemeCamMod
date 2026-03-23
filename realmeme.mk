25805362cb74ef5c496dd5435a3f524003783b39
itzsjdude <itzsjdude@users.noreply.github.com>
2026-03-23 16:08:14 +0530

    Declared RomUpdateProviderShim android_app and added to PRODUCT_PACKAGES

--- realmeme.mk
+++ realmeme.mk
@@ -13,6 +13,7 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # --- Auto-Generated PRODUCT_PACKAGES list ---
 PRODUCT_PACKAGES += \
+    RomUpdateProviderShim \\
     OplusCamera \\
     libarcsoft_hdr_denoise_api \
     libarcsoft_dualcam_refocus_uw \
