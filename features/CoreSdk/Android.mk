7a297ba348c61d1343071560a3abf77e5cac5195
itzsjdude <itzsjdude@users.noreply.github.com>
2026-03-23 23:11:08 +0530

    Fix: Replace Android.bp with conditional Android.mk inside CoreSdk

---
+++ features/CoreSdk/Android.mk
@@ -0,0 +1,24 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := com.oplus.camera.unit.sdk
+LOCAL_MODULE_TAGS := optional
+
+ifeq ($(USE_8PRO_CORE),true)
+LOCAL_SRC_FILES := $(call all-java-files-under, 8pro/com.oplus.camera.unit.sdk/sources)
+else
+LOCAL_SRC_FILES := $(call all-java-files-under, 7pro/com.oplus.camera.unit.sdk)
+endif
+
+LOCAL_JAVA_LIBRARIES := framework oplus-framework
+include $(BUILD_JAVA_LIBRARY)
+
+# Adapter declaration only for 8pro
+ifeq ($(USE_8PRO_CORE),true)
+include $(CLEAR_VARS)
+LOCAL_MODULE := com.oplus.camera.unit.sdk.adapter
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := $(call all-java-files-under, 8pro/com.oplus.camera.unit.sdk.adapter/sources)
+LOCAL_JAVA_LIBRARIES := framework oplus-framework com.oplus.camera.unit.sdk
+include $(BUILD_JAVA_LIBRARY)
+endif
--- features/CoreSdk/core_sdk.mk
+++ features/CoreSdk/core_sdk.mk
@@ -1,5 +1,5 @@
-ifeq ($(USE_8PRO_CORE),true)
-PRODUCT_PACKAGES += com.oplus.camera.unit.sdk_8pro com.oplus.camera.unit.sdk.adapter_8pro
-else
 PRODUCT_PACKAGES += com.oplus.camera.unit.sdk
+
+ifeq ($(USE_8PRO_CORE),true)
+PRODUCT_PACKAGES += com.oplus.camera.unit.sdk.adapter
 endif
