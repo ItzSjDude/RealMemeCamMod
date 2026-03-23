311a1322c3e37350ca3f86078eb9cc13753e4f62
itzsjdude <itzsjdude@users.noreply.github.com>
2026-03-23 08:05:16 +0530

    Removed Magisk specific scripts from extras folder

--- extras/RM8pro_TONEMAP/service.sh
+++
@@ -1,5 +0,0 @@
-#!/system/bin/sh
-
-mkdir -p /odm/etc/camera/pf 2>/dev/null
-rm -rf /odm/etc/camera/pf/PerfectColorSet 2>/dev/null
-ln -s /vendor/etc/camera/pf/PerfectColorSet /odm/etc/camera/pf/PerfectColorSet 2>/dev/null
