a1d131444abda6a960a73971ba9ee87aaf7d027d
itzsjdude <itzsjdude@users.noreply.github.com>
2026-03-23 16:36:10 +0530

    Fixed double-backslash syntax error in realmeme.mk

--- realmeme.mk
+++ realmeme.mk
@@ -13,8 +13,8 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # --- Auto-Generated PRODUCT_PACKAGES list ---
 PRODUCT_PACKAGES += \
-    RomUpdateProviderShim \\
-    OplusCamera \\
+    RomUpdateProviderShim \
+    OplusCamera \
     libarcsoft_hdr_denoise_api \
     libarcsoft_dualcam_refocus_uw \
     libstfaceunlockocl \
