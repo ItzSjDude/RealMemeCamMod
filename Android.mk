c9a793cd1fa5d8e4743a590cc70649f4ea774014
itzsjdude <itzsjdude@users.noreply.github.com>
2026-03-23 09:31:14 +0530

    Emptied Android.mk to resolve duplicate definition of libAlgoInterface

--- Android.mk
+++ Android.mk
@@ -1,11 +1,1 @@
-# Realmeme Camera Mod - Android.mk MakeFile
-LOCAL_PATH := \$(call my-dir)
-
-include \$(CLEAR_VARS)
-LOCAL_MODULE := libAlgoInterface
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX := .so
-LOCAL_SRC_FILES := odm/lib64/libAlgoInterface.so
-LOCAL_MULTILIB := 64
-LOCAL_CHECK_ELF_FILES := false
-include \$(BUILD_PREBUILT)
+LOCAL_PATH := $(call my-dir)
