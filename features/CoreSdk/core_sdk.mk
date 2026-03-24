0842fae8eea223160132b94795210273cf4052fc
itzsjdude <itzsjdude@users.noreply.github.com>
2026-03-24 09:43:44 +0530

    Fix: Re-convert CoreSdk to Android.bp and update permissions

--- features/CoreSdk/core_sdk.mk
+++ features/CoreSdk/core_sdk.mk
@@ -1,10 +1,9 @@
-PRODUCT_PACKAGES += com.oplus.camera.unit.sdk
-
 ifeq ($(USE_8PRO_CORE),true)
-PRODUCT_PACKAGES += com.oplus.camera.unit.sdk.adapter
+PRODUCT_PACKAGES += com.oplus.camera.unit.sdk_8pro com.oplus.camera.unit.sdk.adapter_8pro
 PRODUCT_COPY_FILES += \
     vendor/realmeme_cam_mod/features/CoreSdk/8pro/com.oplus.camera.unit.sdk.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.oplus.camera.unit.sdk.xml
 else
+PRODUCT_PACKAGES += com.oplus.camera.unit.sdk
 PRODUCT_COPY_FILES += \
     vendor/realmeme_cam_mod/features/CoreSdk/7pro/com.oplus.camera.unit.sdk.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.oplus.camera.unit.sdk.xml
 endif
