10663daa912f5bf88680cb47d558e6e957b6412d
itzsjdude <itzsjdude@users.noreply.github.com>
2026-03-24 07:25:26 +0530

    Fix: Add permissions XML and update core_sdk.mk for dynamic copying

--- features/CoreSdk/core_sdk.mk
+++ features/CoreSdk/core_sdk.mk
@@ -2,4 +2,9 @@ PRODUCT_PACKAGES += com.oplus.camera.unit.sdk
 
 ifeq ($(USE_8PRO_CORE),true)
 PRODUCT_PACKAGES += com.oplus.camera.unit.sdk.adapter
+PRODUCT_COPY_FILES += \
+    vendor/realmeme_cam_mod/features/CoreSdk/8pro/com.oplus.camera.unit.sdk.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.oplus.camera.unit.sdk.xml
+else
+PRODUCT_COPY_FILES += \
+    vendor/realmeme_cam_mod/features/CoreSdk/7pro/com.oplus.camera.unit.sdk.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.oplus.camera.unit.sdk.xml
 endif
