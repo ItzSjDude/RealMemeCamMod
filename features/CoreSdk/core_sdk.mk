22d2e42edd4546c2aa3f12c593f00da17b5cb45c
itzsjdude <itzsjdude@users.noreply.github.com>
2026-03-24 09:55:18 +0530

    Fix: Remove redundant _8pro suffix from adapter module name

--- features/CoreSdk/core_sdk.mk
+++ features/CoreSdk/core_sdk.mk
@@ -1,5 +1,5 @@
 ifeq ($(USE_8PRO_CORE),true)
-PRODUCT_PACKAGES += com.oplus.camera.unit.sdk_8pro com.oplus.camera.unit.sdk.adapter_8pro
+PRODUCT_PACKAGES += com.oplus.camera.unit.sdk_8pro com.oplus.camera.unit.sdk.adapter
 PRODUCT_COPY_FILES += \
     vendor/realmeme_cam_mod/features/CoreSdk/8pro/com.oplus.camera.unit.sdk.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.oplus.camera.unit.sdk.xml
 else
