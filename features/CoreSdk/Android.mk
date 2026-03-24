cc16c1b986b946b3ade0e207ed6d424d576cc924
itzsjdude <itzsjdude@users.noreply.github.com>
2026-03-24 08:17:17 +0530

    Fix: Add LOCAL_PRODUCT_MODULE := true to install to product framework

--- features/CoreSdk/Android.mk
+++ features/CoreSdk/Android.mk
@@ -11,6 +11,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, 7pro/com.oplus.camera.unit.sdk)
 endif
 
 LOCAL_JAVA_LIBRARIES := framework oplus-framework
+LOCAL_PRODUCT_MODULE := true
 include $(BUILD_JAVA_LIBRARY)
 
 # Adapter declaration only for 8pro
@@ -20,5 +21,6 @@ LOCAL_MODULE := com.oplus.camera.unit.sdk.adapter
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(call all-java-files-under, 8pro/com.oplus.camera.unit.sdk.adapter/sources)
 LOCAL_JAVA_LIBRARIES := framework oplus-framework com.oplus.camera.unit.sdk
+LOCAL_PRODUCT_MODULE := true
 include $(BUILD_JAVA_LIBRARY)
 endif
