739b3ba5f074a68faad614ae6b349783f54a4554
itzsjdude <itzsjdude@users.noreply.github.com>
2026-03-24 16:53:18 +0530

    oplus-frameworks: Create RefStaticObject stub and Inclusive strip imports for dummied classes

---
+++ oplus-frameworks/opcore-fwk/src/com/oplus/internal/reflect/RefStaticObject.java
@@ -0,0 +1,6 @@
+package com.oplus.internal.reflect;
+public class RefStaticObject<T> {
+    public T get() { return null; }
+    public void set(T value) {}
+    public T get(Object obj) { return null; }
+}
