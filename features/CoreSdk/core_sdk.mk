203011b85f090c31be413401d9171cc4aed8fbfd
itzsjdude <itzsjdude@users.noreply.github.com>
2026-03-24 10:02:41 +0530

    Fix: Split CoreSdk into separate namespaces for absolute naming

--- features/CoreSdk/core_sdk.mk
+++ features/CoreSdk/core_sdk.mk
@@ -1,9 +1,13 @@
 ifeq ($(USE_8PRO_CORE),true)
-PRODUCT_PACKAGES += com.oplus.camera.unit.sdk_8pro com.oplus.camera.unit.sdk.adapter
+PRODUCT_SOONG_NAMESPACES += vendor/realmeme_cam_mod/features/CoreSdk/8pro
+PRODUCT_PACKAGES += com.oplus.camera.unit.sdk.adapter
 PRODUCT_COPY_FILES += \
     vendor/realmeme_cam_mod/features/CoreSdk/8pro/com.oplus.camera.unit.sdk.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.oplus.camera.unit.sdk.xml
 else
-PRODUCT_PACKAGES += com.oplus.camera.unit.sdk
+PRODUCT_SOONG_NAMESPACES += vendor/realmeme_cam_mod/features/CoreSdk/7pro
 PRODUCT_COPY_FILES += \
     vendor/realmeme_cam_mod/features/CoreSdk/7pro/com.oplus.camera.unit.sdk.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.oplus.camera.unit.sdk.xml
 endif
+
+# Common module packaging for both
+PRODUCT_PACKAGES += com.oplus.camera.unit.sdk
