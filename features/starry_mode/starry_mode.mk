4db20833f824793f45875601a930bc46477fdc2f
itzsjdude <itzsjdude@users.noreply.github.com>
2026-03-24 10:45:31 +0530

    Fix: Stage remaining changes before LFS migration

---
+++ features/starry_mode/starry_mode.mk
@@ -0,0 +1,7 @@
+
+ifeq ($(ENABLE_StarryMode),true)
+
+PRODUCT_COPY_FILES += \
+    vendor/realmeme_cam_mod/features/starry_mode/system/odm/lib64/libStarMode.so:$(TARGET_COPY_OUT_ODM)/lib64/libStarMode.so
+
+endif
