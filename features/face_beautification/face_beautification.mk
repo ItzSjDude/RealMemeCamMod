2199ca1eb78bb4a964debf85098a621f50353e11
itzsjdude <itzsjdude@users.noreply.github.com>
2026-03-26 23:15:24 +0530

    fix(build): resolve 'missing endif' in face_beautification.mk

--- features/face_beautification/face_beautification.mk
+++ features/face_beautification/face_beautification.mk
@@ -1,9 +1,9 @@
 
 PRODUCT_COPY_FILES += \
     vendor/realmeme_cam_mod/features/face_beautification/system/odm/etc/camera/fb_default:$(TARGET_COPY_OUT_ODM)/etc/camera/fb_default \
-    vendor/realmeme_cam_mod/features/face_beautification/system/odm/etc/camera/fb_model:$(TARGET_COPY_OUT_ODM)/etc/camera/fb_model \
+    vendor/realmeme_cam_mod/features/face_beautification/system/odm/etc/camera/fb_model:$(TARGET_COPY_OUT_ODM)/etc/camera/fb_model
 
 ifeq ($(ENABLE_FaceBeautification),true)
 PRODUCT_COPY_FILES += \
-    vendor/realmeme_cam_mod/features/face_beautification/system/odm/etc/camera/megvii:$(TARGET_COPY_OUT_ODM)/etc/camera/megvii \
+    vendor/realmeme_cam_mod/features/face_beautification/system/odm/etc/camera/megvii:$(TARGET_COPY_OUT_ODM)/etc/camera/megvii
 endif
