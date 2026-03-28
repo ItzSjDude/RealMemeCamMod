fbd0cf2b1c4013f4cecd76fee5b791f4a70a6d6b
itzsjdude <itzsjdude@users.noreply.github.com>
2026-03-28 09:32:00 +0530

    CoreSdk: Resolve CameraConfigure resolution error and consolidate OCS SDK to system_ext
    - Update permission XML files to use /system_ext/framework/ paths
    - Remove redundant /product partition copy instructions from CoreSdk/core_sdk.mk
    - Update CoreSdk/7pro/Android.bp for system_ext consistency
    - Delete redundant product/etc/permissions/com.oplus.camera.unit.sdk_product.xml

--- CoreSdk/core_sdk.mk
+++ CoreSdk/core_sdk.mk
@@ -1,12 +1,8 @@
 ifeq ($(USE_8PRO_CORE),true)
 PRODUCT_SOONG_NAMESPACES += vendor/realmeme_cam_mod/CoreSdk/8pro
 PRODUCT_PACKAGES += com.oplus.camera.unit.sdk.adapter
-PRODUCT_COPY_FILES += \
-    vendor/realmeme_cam_mod/CoreSdk/8pro/com.oplus.camera.unit.sdk.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.oplus.camera.unit.sdk.xml
 else
 PRODUCT_SOONG_NAMESPACES += vendor/realmeme_cam_mod/CoreSdk/7pro
-PRODUCT_COPY_FILES += \
-    vendor/realmeme_cam_mod/CoreSdk/7pro/com.oplus.camera.unit.sdk.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.oplus.camera.unit.sdk.xml
 endif
 
 # Common module packaging for both
