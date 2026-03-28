81542716370a4e7ab1215454e91179a03cfaed0d
itzsjdude <itzsjdude@users.noreply.github.com>
2026-03-28 22:15:41 +0530

    perfect_color: generalized copy rules and synced missing pfb_bin folder

--- features/perfect_color/perfect_color.mk
+++ features/perfect_color/perfect_color.mk
@@ -1,3 +1,2 @@
-# Copy directory contents
-PRODUCT_COPY_FILES += $(foreach f,$(shell find vendor/realmeme_cam_mod/features/perfect_color/system/odm/etc/camera/filters_lut -type f),$(f):$(subst vendor/realmeme_cam_mod/features/perfect_color/system/odm/,$(TARGET_COPY_OUT_ODM)/,$(f)))
-PRODUCT_COPY_FILES += $(foreach f,$(shell find vendor/realmeme_cam_mod/features/perfect_color/system/odm/etc/camera/filters_res -type f),$(f):$(subst vendor/realmeme_cam_mod/features/perfect_color/system/odm/,$(TARGET_COPY_OUT_ODM)/,$(f)))
+# Perfect Color Feature - Copy all files to ODM partition
+PRODUCT_COPY_FILES += $(foreach f,$(shell find vendor/realmeme_cam_mod/features/perfect_color/system/odm -type f),$(f):$(subst vendor/realmeme_cam_mod/features/perfect_color/system/odm/,$(TARGET_COPY_OUT_ODM)/,$(f)))
